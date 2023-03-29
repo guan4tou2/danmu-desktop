fbd6fe101826fe2108091278004006102a976414
guantou <mole30608@gmail.com>
2023-03-29 16:24:57 +0800

    Update start.sh

--- server/start.sh
+++ server/start.sh
@@ -1,2 +1,2 @@
 docker build . -t danmu-server
-docker run -p 3000:3000 -p 4000:4000 -p 5000:5000 -name danmu danmu-server
+docker run -p 3000:3000 -p 4000:4000 -p 5000:5000 --name danmu danmu-server
